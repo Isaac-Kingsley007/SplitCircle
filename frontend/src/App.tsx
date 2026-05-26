50e53d2ad4a676d64df7744e19b7b4129f3acf0f
Isaac-Kingsley007 <160049323+Isaac-Kingsley007@users.noreply.github.com>
2026-05-27 01:36:32 +0530

    Created Frontend React Project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return(
+    <div>
+      <p className="text-red-400 text-xl">Hello World!</p>
+    </div>
+  )
+}
---
+++ frontend/src/route.ts
@@ -0,0 +1,9 @@
+import {createBrowserRouter} from 'react-router'
+import App from './App'    
+
+export default createBrowserRouter([
+    {
+        index: true,
+        Component: App,
+    }
+]);
