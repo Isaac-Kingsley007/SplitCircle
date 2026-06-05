773651bf63cc6ad9a2dfb6838ecc9ddba76680ec
Isaac-Kingsley007 <160049323+Isaac-Kingsley007@users.noreply.github.com>
2026-06-06 02:37:13 +0530

    added craeted time field to tables

--- backend/db/init.js
+++ backend/db/init.js
@@ -4,7 +4,8 @@ await sql`
     CREATE TABLE IF NOT EXISTS users (
         user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         user_name VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT NOW()
     );
 `
 
@@ -12,7 +13,8 @@ await sql`
     CREATE TABLE IF NOT EXISTS splits(
         split_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         split_name VARCHAR(255) NOT NULL,
-        created_by INT REFERENCES users(user_id)
+        created_by INT REFERENCES users(user_id),
+        created_at TIMESTAMPTZ DEFAULT NOW()
     );
 `
 
@@ -21,7 +23,8 @@ await sql`
         expense_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         expense_name VARCHAR(255) NOT NULL,
         expense_amount DECIMAL(10, 2) NOT NULL,
-        split_id INT REFERENCES splits(split_id)
+        split_id INT REFERENCES splits(split_id),
+        created_at TIMESTAMPTZ DEFAULT NOW()
     );
 `
 
@@ -29,6 +32,7 @@ await sql`
     CREATE TABLE IF NOT EXISTS user_splits(
         user_id INT REFERENCES users(user_id),
         split_id INT REFERENCES splits(split_id),
+        created_at TIMESTAMPTZ DEFAULT NOW(),
         PRIMARY KEY (user_id, split_id)
     );
 `
